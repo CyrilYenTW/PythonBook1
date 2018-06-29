3d8947002d86bd6dfdc7c8f011397ab99cd4cf12
Cyril Yen <cyril_yen@bizlinktech.com>
2018-06-29 08:57:31 +0800

    Edit Readme.txt

--- Practice/2-2.py
+++ Practice/2-2.py
@@ -1,6 +1,16 @@
 
-print("Hello World")
+# 2-1 将一条消息存储到变量中，再将其打印出来。
+message = "Hello World"
 
-message = "Hello Python!!"
+print(message)
+
+
+
+# 2-2 将一条消息存储到变量中，将其打印出来；再将变量的值修改为一条新消息，并将其打印出来。
+message = "Message One"
+
+print(message)
+
+message = "Hello Python"
 
 print(message)
