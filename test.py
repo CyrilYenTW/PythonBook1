ad8e0b5e54d3a70805a3ee933807b7faeadcd168
Cyril Yen <cyril_yen@bizlinktech.com>
2018-06-29 17:13:30 +0800

    test someting

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-cars = ['BMW', 'Toyota', 'Benz', 'Subaru', 'Audi']
-print(len(cars)) # 5
+
+for x in range(1, 100):
+	for y in range(1, 100):
+		x_word = '%02d' % x
+		y_word = '%02d' % y
+		r_word = '%02d' % (x * y)
+		print(x_word + ' * ' + y_word + ' = ' + r_word)
